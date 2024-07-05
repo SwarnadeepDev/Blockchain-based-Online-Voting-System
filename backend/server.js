cb9f2fb578a7fadb693d31933a18b1de7a53366b
Swarnadeep@1234 <debswarnadeepdevelopment@gmail.com>
2024-07-05 18:33:17 +0530

    updating server code and package.json for deploying

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const apiRoutes = require('./routes/api');
 
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
